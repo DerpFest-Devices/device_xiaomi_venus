5f68cf27edd89c260e2a72727207a6e658214a58
VortexHD <vortexhdwhite@gmail.com>
2021-11-01 15:37:05 +0000

    venus: Update build fingerprint
    Update to latest China stable build fingerprint

--- derp_venus.mk
+++ derp_venus.mk
@@ -27,3 +27,6 @@ PRODUCT_MODEL := M2011K2C
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build fingerprint
+BUILD_FINGERPRINT := "Xiaomi/venus/venus:11/RKQ1.200928.002/V12.5.13.0.RKBCNXM:user/release-keys"
